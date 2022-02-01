40d03762972fc57f3eebcdd72cac68be9dda2bb5
Maximilian Edmunds <edmunds.m@etoncollege.org.uk>
2022-02-01 22:06:29 +0000

    Initial Commit

---
+++ AbsenceRecorder/AbsenceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorderApp.swift
+//  AbsenceRecorder
+//
+//  Created by Max Edmunds on 01/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
