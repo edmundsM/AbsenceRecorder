2be7824a08c217d1aa7006cb8ef542d4172f26ea
Maximilian Edmunds <edmunds.m@etoncollege.org.uk>
2022-02-01 22:27:05 +0000

    Added student and division

---
+++ AbsenceRecorder/Model/Division.swift
@@ -0,0 +1,18 @@
+//
+//  Division.swift
+//  AbsenceRecorder
+//
+//  Created by Max Edmunds on 01/02/2022.
+//
+
+import Foundation
+
+
+class Division {
+    let code: String
+    var students: [Student] = []
+    
+    init(code: String) {
+        self.code = code
+    }
+}
---
+++ AbsenceRecorder/Model/Student.swift
@@ -0,0 +1,20 @@
+//
+//  Student.swift
+//  AbsenceRecorder
+//
+//  Created by Max Edmunds on 01/02/2022.
+//
+
+import Foundation
+
+class Student {
+    let forename: String
+    let surname: String
+    let birthday: Date
+    
+    init(forename: String, surname: String, birthday: Date) {
+        self.forename = forename
+        self.surname = surname
+        self.birthday = birthday
+    }
+}
